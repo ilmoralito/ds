148e64631b679fb77faab3b5ca8aab021a733924
Mario Martinez <amakenadog@gmail.com>
2013-05-18 10:30:25 -0600

    first commit

---
+++ src/templates/artifacts/Controller.groovy
@@ -0,0 +1,60 @@
+@artifact.package@class @artifact.name@ {
+
+	static defaultAction = "list"
+	static allowedMethods = [list:"GET", create:["GET", "POST"]]
+
+    def list() {
+    	def domainClassInstance = DomainClass.list()
+    	[domainClassInstance:domainClassInstance]
+    }
+
+    def create() {
+    	if (request.get) {
+    		[domainClassInstance:new domainClassInstance(params)]
+    	} else {
+    		def domainClassInstance = new DomainClass(params)
+
+    		if (!domainClassInstance.save()) {
+    			return [domainClassInstance:domainClassInstance]
+    		}
+
+    		flash.message = "data.saved"
+    	}
+    }
+
+    def show(Integer id) {
+    	def domainClassInstance = DomainClass.get(id)
+
+    	if (!domainClassInstance) {
+    		response.sendError 404
+    		return false
+    	}
+
+    	if (request.get) {
+    		return [domainClassInstance:domainClassInstance]
+    	} else if (request.post) {
+    		domainClassInstance.properties = params
+
+    		if (!domainClassInstance.save()) {
+    			return [domainClassInstance:domainClassInstance]
+    		}
+
+    		flash.message = "data.saved"
+    	}
+    }
+
+    def delete(Integer id) {
+    	def domainClassInstance = DomainClass.get(id)
+
+    	if (!domainClassInstance) {
+    		response.sendError 404
+    		return false
+    	}
+
+    	domainClassInstance.delete()
+
+    	flash.message = "data.deleted"
+    	redirect action:"list"
+    }
+
+}
---
+++ src/templates/artifacts/DomainClass.groovy
@@ -0,0 +1,24 @@
+@artifact.package@class @artifact.name@ {
+
+
+
+	Date dateCreated
+	Date lastUpdated
+
+    static constraints = {
+
+    }
+
+    static namedQueries = {
+
+    }
+
+    static mapping = {
+    	version false
+    }
+
+    String toString() {
+
+    }
+
+}
