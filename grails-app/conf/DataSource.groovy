5051253a64ad5ea56ba4f02aa7a8fc78874f8730
mario martinez <amakenadog@gmail.com>
2014-06-03 10:37:03 -0600

    Added postgre db support

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -26,7 +26,12 @@ environments {
   production {
     dataSource {
       dbCreate = "update"
-      url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+      driverClassName = "org.postgresql.Driver"
+      dialect = org.hibernate.dialect.PostgreSQLDialect
+      uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+      url = "jdbc:postgresql://"+uri.host+uri.path
+      username = uri.userInfo.split(":")[0]
+      password = uri.userInfo.split(":")[1]
       pooled = true
       properties {
         maxActive = -1
