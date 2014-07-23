6d09a1591501e76a27b655bccda89dc296b3312c
mario martinez <amakenadog@gmail.com>
2014-07-23 13:47:37 -0600

    Tab 2

--- grails-app/domain/ni/edu/uccleon/Request.groovy
+++ grails-app/domain/ni/edu/uccleon/Request.groovy
@@ -3,102 +3,100 @@ package ni.edu.uccleon
 import org.hibernate.transform.AliasToEntityMapResultTransformer
 
 class Request implements Serializable {
+  Date dateOfApplication
+  String classroom
+  String school
+  String description
+  Integer datashow
+  String type
 
-    Date dateOfApplication
-    String classroom
-    String school
-    String description
-    Integer datashow
-    String type
+  Boolean audio
+  Boolean screen
+  Boolean internet
 
-    Boolean audio
-    Boolean screen
-    Boolean internet
-
-    String status = "pending"
+  String status = "pending"
 
 	Date dateCreated
 	Date lastUpdated
 
-    static constraints = {
-        dateOfApplication blank:false, validator: {val, obj ->
-            def today = new Date()
-            def minDate = today + 2
+  static constraints = {
+    dateOfApplication blank:false, validator: {val, obj ->
+      def today = new Date()
+      def minDate = today + 2
 
-            if (obj.type == "common") {
-                return val >= minDate.clearTime()
-            } else {
-                return val >= today.clearTime()
-            }
-        }
-        classroom blank:false
-        school blank:false
-        description nullable:true, maxSize:10000
-        datashow nullable:true//, range:0..grailsApplication.config.ni.edu.uccleon.datashows
-        type inList:["common", "express"], maxSize:255
-        audio nullable:true
-        screen nullable:true
-        internet nullable:true
-        status inList:["pending", "attended", "absent", "canceled"]
+      if (obj.type == "common") {
+        return val >= minDate.clearTime()
+      } else {
+        return val >= today.clearTime()
+      }
+    }
+    classroom blank:false
+    school blank:false
+    description nullable:true, maxSize:10000
+    datashow nullable:true//, range:0..grailsApplication.config.ni.edu.uccleon.datashows
+    type inList:["common", "express"], maxSize:255
+    audio nullable:true
+    screen nullable:true
+    internet nullable:true
+    status inList:["pending", "attended", "absent", "canceled"]
+  }
+
+  static namedQueries = {
+    listByUser {user ->
+      eq "user", user
     }
 
-    static namedQueries = {
-        listByUser {user ->
-            eq "user", user
-        }
-
-        listByRole {role ->
-            user {
-                eq "role", role
-            }
-        }
-
-        todayRequest {
-            def today = new Date()
-
-            ge "dateOfApplication", today.clearTime()
-            le "dateOfApplication", today.clearTime()
-        }
-
-        requestFromTo { from, to ->
-            ge "dateOfApplication", from.clearTime()
-            le "dateOfApplication", to.clearTime()
-        }
-
-        //reports
-        requestsBy { property ->
-            projections {
-                groupProperty property, "property"
-                count property, "count"
-            }
-
-            order("count", "desc")
-            resultTransformer(AliasToEntityMapResultTransformer.INSTANCE)
-        }
-
-        requestsByBlocks {
-            projections {
-                hours {
-                    groupProperty "block", "property"
-                    count "block", "count"
-                }
-            }
-
-            order("count", "desc")
-            resultTransformer(AliasToEntityMapResultTransformer.INSTANCE)
-        }
-
+    listByRole {role ->
+      user {
+        eq "role", role
+      }
     }
 
-    static belongsTo = [user:User]
-    static hasMany = [hours:Hour]
+    todayRequest {
+      def today = new Date()
 
-    static mapping = {
-    	version false
+      ge "dateOfApplication", today.clearTime()
+      le "dateOfApplication", today.clearTime()
     }
 
-    String toString() {
-        "the $dateOfApplication in $classroom from $user to $school"
+    requestFromTo { from, to ->
+      ge "dateOfApplication", from.clearTime()
+      le "dateOfApplication", to.clearTime()
     }
+
+    //reports
+    requestsBy { property ->
+      projections {
+        groupProperty property, "property"
+        count property, "count"
+      }
+
+      order("count", "desc")
+      resultTransformer(AliasToEntityMapResultTransformer.INSTANCE)
+    }
+
+    requestsByBlocks {
+      projections {
+        hours {
+          groupProperty "block", "property"
+          count "block", "count"
+        }
+      }
+
+      order("count", "desc")
+      resultTransformer(AliasToEntityMapResultTransformer.INSTANCE)
+    }
+  }
+
+  static belongsTo = [user:User]
+  static hasMany = [hours:Hour]
+
+  static mapping = {
+  	version false
+  }
+
+  String toString() {
+    "the $dateOfApplication in $classroom from $user to $school"
+  }
 
 }
