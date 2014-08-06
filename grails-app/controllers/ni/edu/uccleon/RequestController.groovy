ba726dcda8bf0ac5df27a1f3d7b87d88c0725201
mario martinez <amakenadog@gmail.com>
2014-08-06 14:03:09 -0600

    Fixed display date when call activity action

--- grails-app/controllers/ni/edu/uccleon/RequestController.groovy
+++ grails-app/controllers/ni/edu/uccleon/RequestController.groovy
@@ -249,7 +249,7 @@ class RequestController {
         }
       }
 
-      [requests:requests, blocks:blocks.call(), day:day]
+      [requests:requests, blocks:blocks.call(), day:day, dateSelected: date]
     }
 
     //REPORTS
