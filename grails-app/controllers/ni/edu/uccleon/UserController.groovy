167d385d1cfb373c761749308154595575c8735c
Mario Martinez <amakenadog@gmail.com>
2015-01-19 11:33:44 -0600

    Removed no needed actions and reindented based in 2tab size

--- grails-app/controllers/ni/edu/uccleon/UserController.groovy
+++ grails-app/controllers/ni/edu/uccleon/UserController.groovy
@@ -2,131 +2,131 @@ package ni.edu.uccleon
 
 class UserController {
 
-    def userService
-    def emailConfirmationService
+  def userService
+  def emailConfirmationService
 
-	static defaultAction = "login"
-    static allowedMethods = [
-    	list:["GET", "POST"],
-    	create:["GET", "POST"],
-    	show:["GET", "POST"],
-      enableDisableUserAccount:"POST",
-      notification:"POST",
-    	delete:"GET",
-    	login:["GET", "POST"],
-    	updatePassword:"POST",
-    	resetPassword:"GET",
-      profile:["GET", "POST"],
-      schoolsAndDepartments:["GET", "POST"],
-      classrooms:["GET", "POST"]
-    ]
+  static defaultAction = "login"
+  static allowedMethods = [
+  	list:["GET", "POST"],
+  	create:["GET", "POST"],
+  	show:["GET", "POST"],
+    enableDisableUserAccount:"POST",
+    notification:"POST",
+  	delete:"GET",
+  	login:["GET", "POST"],
+  	updatePassword:"POST",
+  	resetPassword:"GET",
+    profile:["GET", "POST"],
+    schoolsAndDepartments:["GET", "POST"],
+    classrooms:["GET", "POST"]
+  ]
 
-    def list() {
-    	def users
+  def list() {
+    def users
 
-        params.max = Math.min(params.int('max') ?: 10, 100)
+    params.max = Math.min(params.int('max') ?: 10, 100)
 
-    	if (request.method == "POST") {
-            def query = "%${params?.query}%"
+  	if (request.method == "POST") {
+      def query = "%${params?.query}%"
 
-    		users = User.listByRole("user").search(query).list(params)
-    	} else {
-            users = User.listByRole("user").list(params)
-        }
-
-        [users:users, usersCount:User.count()]
+      users = User.listByRole("user").search(query).list(params)
+  	} else {
+      users = User.listByRole("user").list(params)
     }
 
-    def create() {
-        if (request.post) {
-            def schools = params.list "schools"
-            def classrooms = params.list "classrooms"
+    [users:users, usersCount:User.count()]
+  }
 
-            def user = new User(
-                email:params?.email,
-                password:"123",
-                fullName:params?.fullName,
-                enabled:true
-            )
+  def create() {
+    if (request.post) {
+      def schools = params.list "schools"
+      def classrooms = params.list "classrooms"
 
-            schools.each { school -> user.addToSchools school }
+      def user = new User(
+        email:params?.email,
+        password:"123",
+        fullName:params?.fullName,
+        enabled:true
+      )
 
-            classrooms.each { classroom -> user.addToClassrooms classroom }
+      schools.each { school -> user.addToSchools school }
 
-            if (!user.save()) {
-                return [user:user]
-            }
+      classrooms.each { classroom -> user.addToClassrooms classroom }
 
-            //notify new user
-            sendMail {
-                to params.email
-                subject "Datashow"
-                html g.render(template:"email", model:[user:user])
-            }
+      if (!user.save()) {
+        return [user:user]
+      }
 
-            flash.message = "Usuario creado y notificacion enviada"
-        }
+      //notify new user
+      sendMail {
+        to params.email
+        subject "Datashow"
+        html g.render(template:"email", model:[user:user])
+      }
+
+      flash.message = "Usuario creado y notificacion enviada"
+    }
+  }
+
+  def show(Integer id) {
+    def user = User.get(id)
+
+    if (!user) { response.sendError 404 }
+
+    if (request.get) {
+      return [user:user]
+    } else if (request.post) {
+      user.properties = params
+
+      if (!user.save()) {
+        return [user:user]
+      }
+
+      flash.message = "data.saved"
+    }
+  }
+
+  def enableDisableUserAccount(Integer id) {
+    def user = User.get(id)
+
+    if (!user) { response.sendError 404 }
+
+    user.properties["enabled"] = !user.enabled
+
+    flash.message = (!user.save()) ? "A ocurrido un error intentalo nuevamente" : "Estado de cuenta actualizado"
+
+    redirect action:"show", id:id
+  }
+
+  def notification(Integer id) {
+    def user = User.get id
+
+    if (!user) {
+      response.sendError 404
     }
 
-    def show(Integer id) {
-        def user = User.get(id)
-
-        if (!user) { response.sendError 404 }
-
-        if (request.get) {
-            return [user:user]
-        } else if (request.post) {
-            user.properties = params
-
-            if (!user.save()) {
-                return [user:user]
-            }
-
-            flash.message = "data.saved"
-        }
+    sendMail {
+      to user.email
+      subject "Datashow"
+      html g.render(template:"email", model:[user:user])
     }
 
-    def enableDisableUserAccount(Integer id) {
-        def user = User.get(id)
+    redirect action:"show", id:id
+  }
 
-        if (!user) { response.sendError 404 }
+  def delete(Integer id) {
+    def user = User.get(id)
 
-        user.properties["enabled"] = !user.enabled
-
-        flash.message = (!user.save()) ? "A ocurrido un error intentalo nuevamente" : "Estado de cuenta actualizado"
-
-        redirect action:"show", id:id
+    if (!user) {
+      response.sendError 404
+      return false
     }
 
-    def notification(Integer id) {
-        def user = User.get id
+    user.delete()
 
-        if (!user) {
-            response.sendError 404
-        }
-
-        sendMail {
-            to user.email
-            subject "Datashow"
-            html g.render(template:"email", model:[user:user])
-        }
-
-        redirect action:"show", id:id
-    }
-
-    def delete(Integer id) {
-        def user = User.get(id)
-
-        if (!user) {
-            response.sendError 404
-            return false
-        }
-
-        user.delete()
-
-        flash.message = "data.deleted"
-        redirect action:"list"
-    }
+    flash.message = "data.deleted"
+    redirect action:"list"
+  }
 
   def profile() {
     def user = User.findByEmail(session?.user?.email)
@@ -203,48 +203,44 @@ class UserController {
     redirect action:(params.path) ?: "password", params:[id:cmd.id]
   }
 
-    def login(String email, String password) {
-      if (request.post) {
-        def user = User.login(email, password).get()
+  def login(String email, String password) {
+    if (request.post) {
+      def user = User.login(email, password).get()
 
-        if (!user) {
-          flash.message = "user.not.found"
-        } else {
-          session.user = user
-          redirect controller:"request", action:"list"
-          return false
-        }
+      if (!user) {
+        flash.message = "user.not.found"
+      } else {
+        session.user = user
+        redirect controller:"request", action:"list"
+        return false
       }
     }
+  }
 
-    def resetPassword(Integer id) {
-    	def user = User.get(id)
+  def resetPassword(Integer id) {
+  	def user = User.get(id)
 
-    	if (!user) {
-    		response.sendError 404
-    	}
+  	if (!user) {
+  		response.sendError 404
+  	}
 
-    	//TODO:generate token of 7 values
-    	user.properties["password"] = "1234567"
+  	//TODO:generate token of 7 values
+  	user.properties["password"] = "1234567"
 
-    	if (!user.save()) {
-    		flash.message = "something.when.wrong"
-    		redirect action:"show", params:[id:id]
-    		return false
-    	}
+  	if (!user.save()) {
+  		flash.message = "something.when.wrong"
+  		redirect action:"show", params:[id:id]
+  		return false
+  	}
 
-    	flash.message = "dato.guardado"
-    	redirect action:"show", params:[id:id]
-    }
+  	flash.message = "dato.guardado"
+  	redirect action:"show", params:[id:id]
+  }
 
-    def logout() {
-        session.user = null
-        redirect action:"login"
-    }
-
-    def welcome() { }
-    def oops() { }
-    def invalid() { }
+  def logout() {
+    session.user = null
+    redirect action:"login"
+  }
 }
 
 class updatePasswordCommand {
