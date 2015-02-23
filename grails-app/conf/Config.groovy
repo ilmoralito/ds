a8af5b7e56d71a6ce44f5b0d2c9ab82b31d38b03
Mario Martinez <amakenadog@gmail.com>
2015-02-23 14:37:01 -0600

    Fixed wrong classrooms code

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -107,7 +107,7 @@ ni.edu.uccleon.cls = [
       [code:"C206", name:"Lab 4"], [code:"C207", name:"Lab 3"], [code:"C208", name:"Lab 2"]
   ],
   D:[[code:"D101"], [code:"D102"], [code:"D103"], [code:"D104"], [code:"D105"], [code:"D109"], [code:"D201"], [code:"D202"], [code:"D203"], [code:"D204"], [code:"D205"], [code:"D206"], [code:"D207"], [code:"D208", name:"Lab 1"]],
-  E:[[code:"E108"], [code:"E112"], [code:"E113", name:"Finanzas"], [code:"E114", name:"Administracion"], [code:"E115"], [code:"E116"], [code:"E117"], [code:"E118"], [code:"E204", name:"Sala de reuniones"], [code:"E208"], [code:"E209"], [code:"E210"], [code:"E211"], [code:"E212"], [code:"E213"], [code:"E214"], [code:"E215"], [code:"E216"], [code:"E217"], [code:"E218"], [code:"E219", name:"Sala de maestros"], [code:"E220", name:"Escuela de manejo"]],
+  E:[[code:"E108"], [code:"E112"], [code:"E113", name:"Finanzas"], [code:"E114", name:"Administracion"], [code:"E115"], [code:"E116"], [code:"E117"], [code:"E118"], [code:"E119", name:"Sala de maestros"], [code:"E204", name:"Sala de reuniones"], [code:"E208"], [code:"E209"], [code:"E210"], [code:"E211"], [code:"E212"], [code:"E213"], [code:"E214"], [code:"E215"], [code:"E216"], [code:"E217"], [code:"E218"], [code:"E219"], [code:"E220", name:"Escuela de manejo"]],
   K:[[code:"K103"], [code:"K104"], [code:"K105"], [code:"K201"], [code:"K202"], [code:"K203"]],
   undefined:[[code:"Afuera de UCC"], [code:"Corredor registro"]]
 ]
