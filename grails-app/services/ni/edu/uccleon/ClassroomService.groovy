d311acdb984dec4dec1206351b658a16d3945e9a
Mario Martinez <amakenadog@gmail.com>
2015-07-10 11:29:58 -0600

    Hotfix classroom size less than 5

--- grails-app/services/ni/edu/uccleon/ClassroomService.groovy
+++ grails-app/services/ni/edu/uccleon/ClassroomService.groovy
@@ -9,7 +9,7 @@ class ClassroomService {
 
   	requests.collect { r ->
     	def code = r.classroom[0]
-    	def k = code in codes[0..4] && r.classroom.size() == 4 ? code : "undefined"
+    	def k = code in codes[0..4] && r.classroom.size() <= 5 ? code : "undefined"
     	def t = classrooms[k].find { it.code == r.classroom }
 
     	r.classroom = t.containsKey("name") ? t.name : t.code
